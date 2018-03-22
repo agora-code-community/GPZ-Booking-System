38a14fa35ec6e8ab022492a492e59e8d8c333093
marc <mac.alicks@gmail.com>
2018-03-22 06:18:21 +0200

    Login and general app layout design implementation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ViewAllComponent } from './components/view-all/view-all.component';
 
+// routing links
+const appRoutes: Routes = [
+
+  { path: 'login', component: LoginComponent },
+  { path: '', component: DashboardComponent },
+  { path: 'view-booking', component: ViewAllComponent },
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    SidemenuComponent,
+    DashboardComponent,
+    ViewAllComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
