a65aa5aa6335ae6e3851550b51bc78d5be6ba2f1
vikwato <vikwatokamanga@rocketmail.com>
2018-04-19 12:58:02 +0200

    Added PGSQL Codeship Configuration

--- server/config/database.php
+++ server/config/database.php
@@ -78,6 +78,19 @@ return [
             'prefix' => '',
         ],
 
+        'pgsql_codeship' => [
+            'driver' => 'pgsql',
+            'host' => 'localhost',
+            'port' => '5432',
+            'database' => 'development',
+            'username' => env('PGUSER'),
+            'password' => env('PGPASSWORD'),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
+
     ],
 
     /*
