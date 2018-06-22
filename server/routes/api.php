d11055608875e78290311fea18a78def13573b4b
vikwato <vikwatokamanga@rocketmail.com>
2018-06-22 13:41:19 +0200

    Authenticated the routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -14,25 +14,31 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['prefix' => 'rooms'], function () {
-    Route::get('all', 'RoomController@index');
-    Route::get('{room}', 'RoomController@select');
-    Route::post('', 'RoomController@store');
-    Route::put('{room}', 'RoomController@update');
-    Route::delete('{room}', 'RoomController@destroy');
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::get('all', 'RoomController@index');
+        Route::get('{room}', 'RoomController@select');
+        Route::post('', 'RoomController@store');
+        Route::put('{room}', 'RoomController@update');
+        Route::delete('{room}', 'RoomController@destroy');
+    });
 });
 
 Route::group(['prefix' => 'events'], function () {
-    Route::get('all', 'EventController@index');
-    Route::get('{event}', 'EventController@select');
-    Route::post('', 'EventController@store');
-    Route::put('{event}', 'EventController@update');
-    Route::delete('{event}', 'EventController@destroy');
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::get('all', 'EventController@index');
+        Route::get('{event}', 'EventController@select');
+        Route::post('', 'EventController@store');
+        Route::put('{event}', 'EventController@update');
+        Route::delete('{event}', 'EventController@destroy');
+    });
 });
 
 Route::group(['prefix' => 'bookings'], function () {
-    Route::post('', 'BookingController@store');
-    Route::put('{booking}', 'BookingController@update');
-    Route::delete('{booking}', 'BookingController@destroy');
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::post('', 'BookingController@store');
+        Route::put('{booking}', 'BookingController@update');
+        Route::delete('{booking}', 'BookingController@destroy');
+    });
 });
 
 Route::group(['prefix' => 'auth'], function(){
