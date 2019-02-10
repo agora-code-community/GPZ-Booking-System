03d7c658ea717a8177fa3346e6b632eeb730d5d9
Chingaipe <mac.alicks@gmail.com>
2019-02-10 19:00:53 +0200

    Preparation for deployment. Solving issues #20 and #21

--- client/src/app/services/rooms.service.ts
+++ client/src/app/services/rooms.service.ts
@@ -4,8 +4,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable()
 export class RoomsService {
 
-  // baseURL = 'https://agora-booking-system.herokuapp.com/api/rooms/';
-  baseURL = 'http://127.0.0.1:8000/api/rooms/';
+  baseURL = 'https://agora-booking-system.herokuapp.com/api/rooms/';
+  // baseURL = 'http://127.0.0.1:8000/api/rooms/';
   contentHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
   constructor(
